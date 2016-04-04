7bd597ea1039018883c74e18250e3a751c474560
Matthew McCoy <matt.d.mccoy@gmail.com>
2016-04-04 11:19:37 -0700

    First Commit

---
+++ one_to_one.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>One to One</title>
+</head>
+<body>
+	<?php
+		for( $i = 1; $i <= 1000; $i += 2) {
+			echo $i . "<br>";
+		}
+	?>
+</body>
+</html>
